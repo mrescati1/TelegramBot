fa076de35588f18608a10b9ff3b0fb8a41bdd23b
mrescati <m.rescati@campus.unimib.it>
2019-06-02 16:13:58 +0200

    first commit

---
+++ first_bot.py
@@ -0,0 +1,17 @@
+import telepot
+import time
+token= '864503331:AAE0RwnKZAXQ1h6dIQsQwlO3iQ0ud42gNfA'
+TelegramBot = telepot.Bot(token)
+updates=TelegramBot.getUpdates()
+n_length=len(updates)
+while True:
+    if len(updates) > n_length:
+        chat_id= updates[-1]['message']['chat']['id']
+        print(updates[-1]['message']['from']['username']+ ' ' + updates[-1]['message']['text'])
+        n_length=len(updates)
+        user_input = raw_input('Michael: ')
+
+        TelegramBot.sendMessage(chat_id, user_input)
+    time.sleep(0.1)
+    updates=TelegramBot.getUpdates()
+    
